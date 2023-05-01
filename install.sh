7b12d10653692dfa929944b1696e3ca3d5cd6fb7
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-05-01 10:55:22 +0000

    refactor

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 nvimInstaller () {
   version=$(nvim --version | awk '/NVIM/ {print $2}')
   if [[ $version == *"-dev"* ]]; then
     echo "Development version of neovim $version is installed!"
-    read -p "Do you want to install latest version LunarVim [Y/n]" vyn
+    read -pr "Do you want to install latest version LunarVim [Y/n]" vyn
     if [ "$vyn" == "n" ]; then
       echo "Skipping..."
     else
-      echo "Installing Lastest Version of LunarVim\n"
+      printf "Installing Lastest Version of LunarVim\n"
       bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
     fi
   else
     echo "Stable version of neovim $version is installed!"
-    read -p "Do you want to install stable version of LunarVim [Y/n]" vyn
+    read -pr "Do you want to install stable version of LunarVim [Y/n]" vyn
     if [ "$vyn" == "n" ]; then
       echo "Skipping..."
     else
-      echo "Installing Stable Version of LunarVim\n"
+      printf "Installing Stable Version of LunarVim\n"
       LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
     fi
   fi
@@ -25,12 +25,12 @@ nvimInstaller () {
 
 # Rofi Installer
 rofiInstaller() {
-  echo "Cloning rofi themes\n"
+  printf "Cloning rofi themes\n"
   git clone https://github.com/lr-tech/rofi-themes-collection.git
   mkdir -p ~/.local/share/rofi/themes/
   cp -r ./rofi-themes-collection/themes/* ~/.local/share/rofi/themes
-  echo "\nYou can choose the rofi theme by running: rofi -show run, and then type rofi-theme-selector"
-  echo "Then choose what theme you like!"
+  printf "\nYou can choose the rofi theme by running: rofi -show run, and then type rofi-theme-selector"
+  printf "Then choose what theme you like!"
   cp ./config/rofi/config.rasi ~/.config/rofi/
 }
 
@@ -38,15 +38,15 @@ installer ()
 {
   echo "---"
   echo "Updating"
-  echo "---\n"
+  printf "---\n"
 
   # Updating
   yay -Syu --noconfirm
   clear
 
   # Installing Packages
-  echo "Installing Packages\n"
-  yay -S nitch lxappearance arc-gtk-theme git papirus-icon-theme thunar alsa-utils alsa-firmware pipewire-audio pipewire-alsa pipewire-pulse bluez xclip feh nitrogen ranger alacritty lazygit atuin ttf-hack-nerd pacman-contrib trash-cli ttf-meslo-nerd httpie atuin zoxide exa bat starship nodejs rofi unzip --noconfirm --needed
+  printf "Installing Packages\n"
+  yay -S nitch unclutter lxappearance arc-gtk-theme git papirus-icon-theme thunar alsa-utils alsa-firmware pipewire-audio pipewire-alsa pipewire-pulse bluez xclip feh nitrogen ranger alacritty lazygit atuin ttf-hack-nerd pacman-contrib trash-cli ttf-meslo-nerd httpie atuin zoxide exa bat starship nodejs rofi unzip --noconfirm --needed
   clear
   
   # Neovim/LunarVim
@@ -57,7 +57,7 @@ installer ()
     echo "Do you want to install nightly version of nvim OR stable version?"
     echo "[1] Nightly Version"
     echo "[2] Stable Version"
-    read -p "Installation process [1/2]" vyn
+    read -pr "Installation process [1/2]" vyn
 
     if [[ "$vyn" -eq 2 ]]; then
       echo "Installation Stable Version of Neovim"
@@ -74,14 +74,14 @@ installer ()
   echo "Do you want to install pnpm throught the AUR helper or the official install script?"
   echo "1: AUR Helper"
   echo "2: Official Installation script"
-  read -p "Installation process [1/2] " pnyn
+  read -pr "Installation process [1/2] " pnyn
 
   case "$pnyn" in
     1) yay -S pnpm --noconfirm
     ;;
     *) 
       echo "Installing pnpm"
-      echo "Please note that you need to follow some steps before you using it!\n"
+      printf "Please note that you need to follow some steps before you using it!\n"
       curl -fsSL https://get.pnpm.io/install.sh | sh -
     ;;
   esac
@@ -104,7 +104,7 @@ installer ()
   fi
 
   # Rofi Installation
-  echo "Coping rofi config\n"
+  printf "Coping rofi config\n"
   if [ -e ~/.config/rofi/config.rasi ]; then
     echo "Rofi config found"
     echo "Moving config.rasi to config.rasi.bak"
@@ -116,9 +116,9 @@ installer ()
   fi
 
   # Installing Zplug
-  echo "Installing Zplug\n"
+  printf "Installing Zplug\n"
   curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-  echo "\nInstalling plugins\n"
+  printf "\nInstalling plugins\n"
   exec zsh
   zplug inatall
 }
@@ -128,7 +128,7 @@ if which yay >/dev/null; then
   installer
 else
   echo "yay not found!"
-  read -p "Do you want to build yay from source? [y/N] " yn
+  read -pr "Do you want to build yay from source? [y/N] " yn
   case "$yn" in
     y* ) 
       git clone https://aur.archlinux.org/yay.git
