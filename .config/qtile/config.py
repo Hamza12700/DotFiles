c1299cf8bdfa68f2c9903ce59bc0385332f22b84
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-07-18 20:41:09 -0700

    remove unnecessary code

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -229,21 +229,6 @@ screens = [
     ),
 ]
 
-
-# Drag floating layouts.
-mouse = [
-    Drag(
-        [mod],
-        "Button1",
-        lazy.window.set_position_floating(),
-        start=lazy.window.get_position(),
-    ),
-    Drag(
-        [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
-    ),
-    Click([mod], "Button2", lazy.window.bring_to_front()),
-]
-
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: list
 follow_mouse_focus = True
