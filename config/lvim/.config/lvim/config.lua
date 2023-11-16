d0a5556c043f83c06ef694ed0e032fa01581e863
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-11-16 09:32:12 -0700

    added `lunarvim` config files

---
+++ config/lvim/.config/lvim/config.lua
@@ -0,0 +1,44 @@
+-- Centers cursor when moving 1/2 page down
+lvim.keys.normal_mode["<C-d>"] = "<C-d>zz"
+
+-- Source the current config file
+lvim.keys.normal_mode["<leader><leader>"] = vim.cmd.so
+
+-- UndoTree
+lvim.keys.normal_mode["<leader>u"] = vim.cmd.UndotreeToggle
+vim.keymap.set("i", "jj", "<Esc>")
+
+vim.cmd.autocmd('BufRead,BufNewFile /home/hamza/.config/waybar/config setfiletype json')
+vim.cmd.autocmd('BufRead,BufNewFile /home/hamza/GitHub/DotFiles/config/waybar/config setfiletype json')
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
+
+lvim.colorscheme = "onedark"
+require('onedark').setup {
+    style = 'darker'
+}
+
+-- Plugins
+lvim.plugins = {
+  { "mbbill/undotree" },
+  { "rust-lang/rust.vim" },
+  { "andweeb/presence.nvim" },
+  { "nvim-treesitter/nvim-treesitter-context" },
+  { "simrat39/rust-tools.nvim",
+    config = function ()
+      require('rust-tools').setup({
+        inlay_hints = true
+      })
+    end
+  },
+  { "navarasu/onedark.nvim" },
+  { "fatih/vim-go" },
+  {
+    'j-hui/fidget.nvim',
+    tag = 'legacy',
+    config = function()
+      require("fidget").setup()
+    end,
+  }
+}
