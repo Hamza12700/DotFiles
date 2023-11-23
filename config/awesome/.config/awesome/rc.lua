666d5b2b69bac875166b457c2a6c0ae3a95f20f2
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-11-23 02:25:06 -0700

    remove `awesome-wm` default notifier and replaced it with `dunst` notification deamon

--- config/awesome/.config/awesome/rc.lua
+++ config/awesome/.config/awesome/rc.lua
@@ -10,8 +10,6 @@ require("awful.autofocus")
 local wibox = require("wibox")
 -- Theme handling library
 local beautiful = require("beautiful")
--- Notification library
-local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
@@ -22,11 +20,7 @@ require("awful.hotkeys_popup.keys")
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
-	naughty.notify({
-		preset = naughty.config.presets.critical,
-		title = "Oops, there were errors during startup!",
-		text = awesome.startup_errors,
-	})
+	awful.spawn.with_shell("notify-send -u critical 'something went wrong' " .. awesome.startup_errors)
 end
 
 -- Handle runtime errors after startup
@@ -38,12 +32,7 @@ do
 			return
 		end
 		in_error = true
-
-		naughty.notify({
-			preset = naughty.config.presets.critical,
-			title = "Oops, an error happened!",
-			text = tostring(err),
-		})
+		awful.spawn.with_shell("notify-send -u critical 'Oops, an error happened!' " .. tostring(err))
 		in_error = false
 	end)
 end
