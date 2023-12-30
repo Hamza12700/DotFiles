c9595de5e46a014e4af4e9d8c687ef484eb18c8d
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-12-30 21:15:52 +0400

    added: wezterm configuration

---
+++ config/wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,20 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Classic Dark (base16)'
+config.font = wezterm.font 'JetBrains Mono'
+config.window_background_opacity = 0.6
+config.window_close_confirmation = 'NeverPrompt'
+config.font_size = 9
+
+return config
