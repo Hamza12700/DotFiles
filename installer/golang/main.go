24c2bd22d26f6dfb20dd98d7213ecfeb9ad0e857
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-01-01 14:29:19 +0400

    remove: redundant code from `main.go`

--- installer/golang/main.go
+++ installer/golang/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 
@@ -17,28 +16,8 @@ func main() {
 	color.Cyan("Golang installer!\n\n")
 
 	color.Green("Checking the AUR Helper")
-	if isCommandAvailable("yay") {
-		successPrint("'yay' AUR Helper found")
-	} else {
-		errorPrint("'yay' AUR Helper not found")
-		color.Yellow("Install it? [y/n]")
-		var yes string
-		fmt.Scanln(&yes)
+	isCommandAvailable("yay")
 
-		if yes == "y" {
-			yayInstall()
-		} else {
-			errorPrint("'yay' AUR Helper isn't installed!")
-			os.Exit(1)
-		}
-	}
-
-	if isCommandAvailable("stow") {
-		successPrint("'stow' command found!")
-		linkConfigDirs()
-	} else {
-		errorPrint("'stow' command not found")
-		requiredPkgInstaller("stow")
-	}
+	isCommandAvailable("stow")
+	linkConfigDirs()
 }
-
