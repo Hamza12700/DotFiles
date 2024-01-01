ea50b88e7ac3d9649b01aa4e0e887b0a5539df00
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-01-01 23:00:24 +0400

    feat: installing the all required packages using `yay` aur helper
    May switch to something else in future like `paur`

--- installer/golang/main.go
+++ installer/golang/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"log"
 	"os"
 	"os/exec"
 
@@ -25,4 +28,33 @@ func main() {
 
 	successPrint("Updating the system")
 	sysCommand("yay", "-Syu")
+
+	file, fileErr := os.Open("packages.txt")
+	if fileErr != nil {
+		log.Fatal(fileErr)
+	}
+
+	buffer := make([]byte, 1024)
+	for {
+		n, err := file.Read(buffer)
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		pkgs := string(buffer[:n])
+
+		if n > 0 {
+			fmt.Println(pkgs)
+		}
+
+		sysCommand("yay", "-S", pkgs, "--needed", "--noconfirm")
+	}
+
+	file.Close()
+	
 }
