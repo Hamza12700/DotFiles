a7d97a8800b019ae6d263adec30dc0e1558914ec
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-12 19:57:32 +0400

    feat: only list files

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -33,7 +33,7 @@ if status --is-interactive
     # KeyBinds
     bind \cz "zi; commandline -f repaint" # [Ctrl + z] - zoxide fizzy finder
     bind -k nul "commandline -f accept-autosuggestion" # [Ctrl + space] - accept-autosuggestion
-    bind \cf "fd -E '.git|node_modules' . | fzf --preview 'bat --color always {}' --header 'Open a file in nvim' --bind 'enter:become(nvim {})'" # [Ctrl + f] - Fizzy find a file/dir and open it in noevim
+    bind \cf "fd -t file -E '.git|node_modules' . | fzf --preview 'bat --color always {}' --header 'Open a file in nvim' --bind 'enter:become(nvim {})'" # [Ctrl + f] - Fizzy find a file/dir and open it in noevim
 
     # Abbreviations
     abbr -a gg lazygit
