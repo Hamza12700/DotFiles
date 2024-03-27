2868f0f8afed34beef5a01901c0da461fc7f417e
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-03-27 22:21:38 +0400

    feat: script to check battery status
    It warns you using notify-send command when the battery is blow 40..=50

---
+++ config/hypr/.config/hypr/scripts/batt_check/src/main.rs
@@ -0,0 +1,42 @@
+use std::{
+  fs::File,
+  io::{BufRead, BufReader},
+  process::{self, Command},
+};
+
+fn main() {
+  loop {
+    let battery_cap_file = File::open("/sys/class/power_supply/BAT0/capacity")
+      .expect("Failed to open battery capacity file");
+
+    let mut reader = BufReader::new(battery_cap_file);
+    let mut battery_level = String::new();
+    if let Err(err) = reader.read_line(&mut battery_level) {
+      eprintln!("Failed to read battery capacity: {}", err);
+      process::exit(1);
+    };
+
+    let battery_level: u8 = battery_level
+      .trim()
+      .parse()
+      .expect("Failed to parse battery capacity to u8");
+
+    match battery_level {
+      40..=50 => get_notified("Battery is almost full", "normal", battery_level),
+      21..=30 => get_notified("Battery is low", "normal", battery_level),
+      10..=20 => get_notified("Battery is almost empty", "critical", battery_level),
+      100 => get_notified("Battery is fully charged", "normal", battery_level),
+      _ => (),
+    };
+
+    std::thread::sleep(std::time::Duration::from_secs(60));
+  }
+}
+
+fn get_notified(title: &str, urgency: &str, batt_cap: u8) {
+  let _ = Command::new("notify-send")
+    .arg(format!("-u \"{}\"", urgency))
+    .arg(title)
+    .arg(format!("{}%", batt_cap))
+    .output();
+}
