299ad342cc44c605ec64565dbce603d3310891af
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-03-29 17:02:31 +0400

    feat: don't send notification when the battery is charging

--- config/hypr/.config/hypr/scripts/batt_check/src/main.rs
+++ config/hypr/.config/hypr/scripts/batt_check/src/main.rs
@@ -16,10 +16,20 @@ fn main() {
   loop {
     let battery_cap_file = File::open("/sys/class/power_supply/BAT0/capacity")
       .expect("Failed to open battery capacity file");
+    let battery_status_file = File::open("/sys/class/power_supply/BAT0/status")
+      .expect("Failed to open battery status file");
 
-    let mut reader = BufReader::new(battery_cap_file);
+    let mut battery_status_buf_reader = BufReader::new(battery_status_file);
+    let mut battery_status = String::new();
+    if let Err(err) = battery_status_buf_reader.read_line(&mut battery_status) {
+      eprintln!("Failed to read battery status: {}", err);
+      process::exit(1);
+    };
+    let battery_status = battery_status.trim();
+
+    let mut battery_cap_buf_reader = BufReader::new(battery_cap_file);
     let mut battery_level = String::new();
-    if let Err(err) = reader.read_line(&mut battery_level) {
+    if let Err(err) = battery_cap_buf_reader.read_line(&mut battery_level) {
       eprintln!("Failed to read battery capacity: {}", err);
       process::exit(1);
     };
@@ -29,19 +39,22 @@ fn main() {
       .parse()
       .expect("Failed to parse battery capacity to u8");
 
-    match battery_level {
-      40..=50 if battery_level_check != BatteryLevels::HalfFull => {
-        get_notified("Battery is half full", battery_level);
-        battery_level_check = BatteryLevels::HalfFull
-      }
-      21..=30 if battery_level_check != BatteryLevels::Low => {
-        get_notified("Battery is low", battery_level);
-        battery_level_check = BatteryLevels::Low
-      }
-      10..=20 => get_notified("Battery is almost empty", battery_level),
-      100 => get_notified("Battery is fully charged", battery_level),
-      _ => (),
-    };
+    if !battery_status.contains("Charging") {
+      match battery_level {
+        40..=50 if battery_level_check != BatteryLevels::HalfFull => {
+          get_notified("Battery is half full", battery_level);
+          battery_level_check = BatteryLevels::HalfFull
+        }
+        21..=30 if battery_level_check != BatteryLevels::Low => {
+          get_notified("Battery is low", battery_level);
+          battery_level_check = BatteryLevels::Low
+        }
+        10..=20 => get_notified("Battery is almost empty", battery_level),
+        _ => (),
+      };
+    } else if battery_level == 100 {
+      get_notified("Battery is fully charged", battery_level)
+    }
 
     std::thread::sleep(std::time::Duration::from_secs(60));
   }
