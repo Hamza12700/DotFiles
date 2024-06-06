2eb053b90a874cbb70d95fae901555940005bb2b
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-06-06 15:26:04 +0000

    refactor: the battery checking script

--- config/hypr/.config/hypr/scripts/btch/src/main.zig
+++ config/hypr/.config/hypr/scripts/btch/src/main.zig
@@ -11,33 +11,31 @@ inline fn notify(title: []const u8, battery_cap: u7) !void {
   _ = c.system(command.ptr);
 }
 
+inline fn getFile(name: []const u8, buffSize: usize) ![]u8 {
+  const file = try fs.openFileAbsolute(name, .{});
+  var buffer: [buffSize]u8 = undefined;
+
+  const read_bytes = try file.read(buffer[0..]);
+  const contents = buffer[0..read_bytes];
+  file.close();
+
+  return contents;
+}
+
 pub fn main() !void {
   const args = std.os.argv;
   if (args.len > 1) {
-    var battery_capacity = try fs.openFileAbsolute("/sys/class/power_supply/BAT0/capacity", .{ .mode = .read_only });
-    var buffer: [5]u8 = undefined;
-    const read_bytes = try battery_capacity.read(buffer[0..]);
-    battery_capacity.close();
-    std.debug.print("Battery: {s}", .{buffer[0..read_bytes]});
+    const contents = try getFile("/sys/class/power_supply/BAT0/capacity", 5);
+    std.debug.print("Battery: {s}", .{contents});
     std.process.exit(0);
   }
   while (true) {
-    var battery_capacity = try fs.openFileAbsolute("/sys/class/power_supply/BAT0/capacity", .{ .mode = .read_only });
-    var battery_state = try fs.openFileAbsolute("/sys/class/power_supply/BAT0/status", .{ .mode = .read_only });
+    const battery_capacity = try getFile("/sys/class/power_supply/BAT0/capacity", 5);
+    const battery_state = try getFile("/sys/class/power_supply/BAT0/status", 8);
 
-    var battery_capacity_buffer: [5]u8 = undefined;
-    const battery_cap_read_bytes = try battery_capacity.read(battery_capacity_buffer[0..]);
-    battery_capacity.close();
+    const battery_state_str = std.mem.trim(u8, battery_state, "\n");
 
-    var battery_state_buffer: [8]u8 = undefined;
-    const battery_state_read_bytes = try battery_state.read(battery_state_buffer[0..]);
-    battery_state.close();
-
-    const battery_state_raw = battery_state_buffer[0..battery_state_read_bytes];
-    const battery_state_str = std.mem.trim(u8, battery_state_raw, "\n");
-
-    const battery_cap_raw = battery_capacity_buffer[0..battery_cap_read_bytes];
-    const battery_cap_trim = std.mem.trim(u8, battery_cap_raw, "\n");
+    const battery_cap_trim = std.mem.trim(u8, battery_capacity, "\n");
     const battery_level = try std.fmt.parseInt(u7, battery_cap_trim, 10);
 
     if (std.mem.eql(u8, battery_state_str, "Full")) {
