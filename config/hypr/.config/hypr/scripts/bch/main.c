4a3f0fa63ed8ad11d4ac3d8385416f32bb958311
Hamza <72913359+Hamza12700@users.noreply.github.com>
2025-02-01 11:56:01 +0000

    feat: improve battery-checking script

--- config/hypr/.config/hypr/scripts/bch/main.c
+++ config/hypr/.config/hypr/scripts/bch/main.c
@@ -1,13 +1,27 @@
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdbool.h>
 
-enum BatteryLeve { None, Half, Low };
+#define TIME 30
+#define BATCAP "/sys/class/power_supply/BAT0/capacity"
+#define BATSTAT "/sys/class/power_supply/BAT0/status"
 
-const char* read_file(const char* filename, char* buffer, size_t buf_size) {
+enum BatteryLevel { None, Half, Low };
+
+// Compare null-terminated strings
+static inline bool str_cmp(const char *s1, const char *s2) {
+  if (strlen(s1) != strlen(s2)) return false;
+
+  for (int i = 0; s1[i] != '\0'; i++) {
+    if (s1[i] != s2[i]) return false;
+  }
+  return true;
+}
+
+const char *read_file(const char *filename, char *buffer, size_t buf_size) {
   FILE *open_file = fopen(filename, "r");
   if (!open_file) {
     fprintf(stderr, "Failed to open file: %s", filename);
@@ -21,7 +35,7 @@ const char* read_file(const char* filename, char* buffer, size_t buf_size) {
   return buffer;
 }
 
-void notify(const char* title, uint8_t battery_level) {
+static inline void notify(const char *title, uint8_t battery_level) {
   char buffer[100];
   sprintf(buffer, "notify-send \"%s\" \"%d\"", title, battery_level);
   system(buffer);
@@ -34,44 +48,52 @@ static inline bool range(uint8_t battery_level, uint8_t low, uint8_t high) {
   return false;
 }
 
-int main(int argv, char* argc[]) {
+int main(int argv, char *argc[]) {
   if (argv > 1) {
     char capacity_buf[8];
-    const char* capacity = read_file("/sys/class/power_supply/BAT0/capacity", capacity_buf, 8);
+    const char *capacity = read_file(BATCAP, capacity_buf, 8);
     char status_buf[10];
-    const char* status = read_file("/sys/class/power_supply/BAT0/status", status_buf, 10);
+    const char *status = read_file(BATSTAT, status_buf, 10);
 
-    if (strcmp(status, "Charging\n") == 0) {
+    if (str_cmp(status, "Charging\n")) {
       puts("Status: Charging");
       printf("Battery: %s", capacity);
       exit(0);
     }
+    puts("Status: Discharging");
     printf("Battery: %s", capacity);
     exit(0);
   }
-  enum BatteryLeve battery_status = None;
+
+  enum BatteryLevel battery_status = None;
 
   while (true) {
     char capacity_buf[8];
-    const char* capacity = read_file("/sys/class/power_supply/BAT0/capacity", capacity_buf, 8);
+    const char *capacity = read_file(BATCAP, capacity_buf, 8);
     char status_buf[10];
-    const char* status = read_file("/sys/class/power_supply/BAT0/status", status_buf, 10);
+    const char *status = read_file(BATSTAT, status_buf, 10);
+
+    if (str_cmp(status, "Full\n")) {
+      notify("Battery is fully charged", 100);
+      sleep(TIME);
+      continue;
+    }
 
     uint8_t battery_level = atoi(capacity);
-    if(battery_level == 100 && strcmp(status, "Full\n") == 0) {
-      notify("Battery is fully charged", battery_level);
-    } else {
-      if (range(battery_level, 40, 50) && battery_status != Half && strcmp(status, "Charging\n") != 0) {
-        notify("Battery is half", battery_level);
-        battery_status = Half;
-      } else if (range(battery_level, 10, 20) && strcmp(status, "Charging\n") != 0 && battery_status != Low) {
-        notify("Battery is almost empty", battery_level);
-        battery_status = Low;
-      } else if (range(battery_level, 1, 9) && strcmp(status, "Charging\n") != 0) {
-        notify("Battery is dead", battery_level);
-      }
+
+    if (range(battery_level, 40, 50) && battery_status != Half &&
+        str_cmp(status, "Charging\n") != true) {
+      notify("Battery is half", battery_level);
+      battery_status = Half;
+    } else if (range(battery_level, 10, 20) &&
+               str_cmp(status, "Charging\n") != true && battery_status != Low) {
+      notify("Battery is almost empty", battery_level);
+      battery_status = Low;
+    } else if (range(battery_level, 1, 9) &&
+               str_cmp(status, "Charging\n") != true) {
+      notify("Battery is dead", battery_level);
     }
-    
-    sleep(30);
+
+    sleep(TIME);
   }
 }
